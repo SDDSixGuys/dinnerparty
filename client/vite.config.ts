64652a1947e55c30db05e2959a963ecf023f9139
Sangeeth T <sangeetht2004@gmail.com>
2026-03-12 15:41:34 -0400

    Fixed vite config and added .env for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8080',
+      '/api': process.env.VITE_API_PROXY_URL || 'http://localhost:3000',
     },
   },
 })
